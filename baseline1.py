ae695c8c5829015b1c96a28c466dfdbf057923a9
RikonYu <grtrbwsword@gmail.com>
2018-01-27 23:01:08 -0500

    making big batch

--- baseline1.py
+++ baseline1.py
@@ -3,16 +3,17 @@ import re
 import gensim
 import keras
 import time
+import sys
+import os
 import utils
 from keras.models import Sequential, Model, load_model
 from keras.layers import Input,Concatenate
 from keras.layers import Reshape,Dense, Dropout, Embedding, LSTM,Flatten,Conv2D,MaxPooling2D
 from keras.optimizers import Adam
-
-TRAINING=1
+TRAINING=int(sys.argv[1])
 sent_len=700
 esize=300
-utils.set_sentlen(700)
+utils.set_sentlen(sent_len)
 tt=time.clock()
 if(TRAINING):
     model=Sequential()
@@ -23,9 +24,12 @@ if(TRAINING):
     model.add(Dense(256))
     model.add(Dense(2,activation='softmax'))
     model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-    model=utils.train(model,10,1024,'baseline1.txt',singular=True)
+    model=utils.train(model,10,-1,'baseline1.txt',singular=True)
     model.save('baseline1.h5')
 else:
     model=load_model('baseline1.h5')
     print(' '.join(utils.test(model,True)))
 print(time.clock()-tt)
+
+
+    
