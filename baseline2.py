d131df728a999210fd07060534cc9bf6aa7c2cd2
RikonYu <grtrbwsword@gmail.com>
2018-04-03 16:28:40 -0400

    baseline2

--- baseline2.py
+++ baseline2.py
@@ -2,28 +2,40 @@ import numpy
 import re
 import utils
 import gensim
+from keras import backend as KTF
 from keras.models import Sequential, Model, load_model
-from keras.layers import Reshape,Dense, Dropout, Embedding, LSTM,Flatten,Conv2D,MaxPooling2D
+from keras.layers import Reshape,Dense, Dropout, Embedding, LSTM,Flatten,Conv2D,MaxPooling2D,Input
 from keras.optimizers import Adam
 TRAINING=1
 esize=300
-sent_len=700
+sent_len=540
 utils.set_sentlen(sent_len)
+def forward(inp):
+    conv1=Conv2D(256,(3,1),activation='sigmoid',padding='valid',input_shape=(sent_len,esize,1))(inp)
+    conv2=Conv2D(256,(2,1),activation='sigmoid',padding='valid')(conv1)
+    lstm1=LSTM(256,activation='sigmoid',return_sequences=True)(conv2)
+    lstm2=LSTM(256,activation='sigmoid')(lstm1)
+    dense=Dense(256,activation='sigmoid')(lstm2)
+    out=Dense(2,activation='softmax')(dense)
+    return out
 if(TRAINING):
-    model=Sequential()
-    model.add(Conv2D(256,(3,esize),activation='relu',padding='valid',input_shape=(sent_len,esize,1)))
-    model.add(Reshape((sent_len-2,256,1)))
-    model.add(Conv2D(256,(3,256),activation='relu',padding='valid'))
-    model.add(Reshape((sent_len-4,256)))
-    model.add(LSTM(256,activation='sigmoid',return_sequences=True))
-    model.add(LSTM(256,activation='sigmoid'))
-    model.add(Dropout(0.5))
-    model.add(Dense(256,activation='sigmoid'))
-    model.add(Dense(2,activation='softmax'))
-    cadam=Adam(lr=0.01)
+    KTF.clear_session()
+    inp=Input(shape=(sent_len*2,esize,1),dtype='float32')
+    toffset=0
+    foffset=0
+    if(len(sys.argv)>2):
+        toffset=int(sys.argv[2])
+        foffset=int(sys.argv[3])
     model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
-    model=utils.train(model,20,512,'baseline2.txt','baseling2-test.txt',singular=True)
-    model.save('baseline2.h5')
+    utils.train(model,8,2048,'baseline2','baseline2-test.txt',True,toffset,foffset)
 else:
-    model=load_model('baseline2.h5')
-    print(' '.join(utils.test(model,True)))
+    min_loss=1000
+    min_pos=-1
+    for i in range(1,9):
+        model=load_model('baseline2_%d.h5'%i)
+        ans=utils.test(model,True)
+        print(' '.join(ans))
+        if(float(ans[3])<min_loss):
+            min_loss=float(ans[3])
+            min_pos=i
+    os.system('cp baseline2_%d.h5 baseline2.h5'%min_pos)
