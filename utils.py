99f51c63157a07b3fdc142fa4125c68aabb879c8
RikonYu <grtrbwsword@gmail.com>
2018-04-15 15:54:55 -0400

    wtf

--- utils.py
+++ utils.py
@@ -347,7 +347,8 @@ def mine_train(default_model,epoch,batch_size,foutname,testoutname,singular,toff
                 p1=numpy.stack([k[1] for k in pos])
                 
                 y=numpy.stack([k[2] for k in ins])
-                history=model.fit([x0,p0,x1,p1],y,epochs=1,verbose=2,validation_split=0)
+                #history=model.fit([x0,p0,x1,p1],y,epochs=1,verbose=2,validation_split=0)
+                history=model.fit([x0,numpy.zeros(p0.shape),x1,numpy.zeros(p1.shape)],y,epochs=1,verbose=2,validation_split=0)
             fout.write(str(history.history['loss'][0]))
             fout.write('\n')
             ins=[]
