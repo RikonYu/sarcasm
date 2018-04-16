ca7330d0868830541b3c7ea0e6689ab8c8dce4fe
RikonYu <grtrbwsword@gmail.com>
2018-04-15 22:17:08 -0400

    fml

--- mine.py
+++ mine.py
@@ -10,7 +10,7 @@ import utils
 from keras import backend as KTF
 from keras.models import Sequential, Model, load_model
 from keras.layers import Input,Concatenate, Layer
-from keras.layers import Reshape,Dense, Dropout, Embedding, LSTM,Flatten,Conv1D,MaxPooling1D,Bidirectional
+from keras.layers import Reshape,Dense, Dropout, Embedding, LSTM,Flatten,Conv2D,MaxPooling2D,Bidirectional
 from keras.optimizers import Adam,SGD
 
 class Attention(Layer):
@@ -113,9 +113,9 @@ sent_len=540
 TRAINING=int(sys.argv[1])
 class mine_model:
     def __init__(self):
-        self.conv_1=Conv1D(128,2,padding='valid',activation='sigmoid')
-        self.conv_2=Conv1D(128,3,padding='valid',activation='sigmoid')
-        self.conv_3=Conv1D(128,4,padding='valid',activation='sigmoid')
+        self.conv1=Conv2D(96,(2,esize),activation='relu')
+        self.conv2=Conv2D(96,(3,esize),activation='relu')
+        self.conv3=Conv2D(96,(4,esize),activation='relu')
         self.lstm1=Bidirectional(LSTM(96,return_sequences=True))
         self.lstm2=Bidirectional(LSTM(96,return_sequences=True))
         self.lstm3=Bidirectional(LSTM(96,return_sequences=True))
@@ -125,23 +125,24 @@ class mine_model:
         
         self.dense=Dense(256,activation='sigmoid')
     def forward(self,inp):
-        inp=Reshape([sent_len,-1])(inp)
-        conv_1=self.conv_1(inp)
-        conv_2=self.conv_2(inp)
-        conv_3=self.conv_3(inp)
-        
-        pool_1=MaxPooling1D(sent_len-1)(conv_1)
-        pool_2=MaxPooling1D(sent_len-2)(conv_2)
-        pool_3=MaxPooling1D(sent_len-3)(conv_3)
+        conv1=self.conv_1(inp)
+        conv2=self.conv_2(inp)
+        conv3=self.conv_3(inp)
+        pool1=MaxPooling2D((sent_len-1,1))(conv1)
+        pool2=MaxPooling2D((sent_len-2,1))(conv2)
+        pool3=MaxPooling2D((sent_len-3,1))(conv3)
         #lstm1=self.lstm1(conv_1)
         #lstm2=self.lstm2(conv_2)
         #lstm3=self.lstm3(conv_3)
         #lstm21=self.lstm2_1(lstm1)
         #lstm22=self.lstm2_2(lstm2)
         #lstm23=self.lstm2_3(lstm3)
-        out=Flatten()(Concatenate(axis=1)([pool_1,pool_2,pool_3]))
+        flat1=Flatten()(pool1)
+        flat2=Flatten()(pool2)
+        flat3=Flatten()(pool3)
+        conc=Concatenate()([flat1,flat2,flat3])
         #out=Concatenate()([lstm21,lstm22,lstm23])
-        out=Dropout(0.25)(out)
+        out=Dropout(0.25)(conc)
         dense=self.dense(out)
         dense=Dropout(0.5)(dense)
         return dense
@@ -176,7 +177,7 @@ if(TRAINING):
     else:
         realdense=Dense(2,activation='softmax',name='realdense')(Concatenate()([left_out[0],left_out[1],right_out[0],right_out[1]]))
         model=Model(inputs=[left_inp,left_pos,right_inp,right_pos],outputs=realdense)
-        model.compile(optimizer=opt,loss='categorical_crossentropy',metrics=['accuracy'])
+        model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
         #if(os.path.isfile('mine_%d.h5'%TRAINING)==False and os.path.isfile('mine_%d.h5'%(TRAINING+1))==False and toffset==0):
         #    model.load_weights('mine-pr.h5',by_name=True)
         utils.mine_train(model,TRAINING,2048,'mine','mine-test.txt',False,toffset,foffset)
